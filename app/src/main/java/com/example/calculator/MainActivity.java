307887444c8f4d22e78eaf88a8d234c29e1f6532
Baychik <ergeshovbaistan@gmail.com>
2023-08-29 11:45:13 +0600

    hw_7

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -10,8 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
-    private String operation = "";
-    private Integer first, second, sum;
+    private String operation = "",minus="-",plus=" ";
+    private Double first,second;
+    private Double sum;
     private Boolean isOperationClick;
 
     @Override
@@ -26,9 +27,9 @@ public class MainActivity extends AppCompatActivity {
         String textButton = ((Button) view).getText().toString();
         if (textButton.equals("AC")) {
             textView.setText("0");
-            first = 0;
-        }else if (textView.getText().toString().contains(".")) {
-            textView.append(".");
+            first = 0.0;
+        } else if (textView.getText().toString().contains(".0")) {
+            textView.append(textButton);
         } else if (textView.getText().toString().equals("0") || isOperationClick) {
             textView.setText(textButton);
         } else {
@@ -41,92 +42,75 @@ public class MainActivity extends AppCompatActivity {
         String textButton = ((Button) view).getText().toString();
         switch (textButton) {
             case "+":
-                first = Integer.valueOf(textView.getText().toString());
+                first = Double.parseDouble(textView.getText().toString());
                 operation = "+";
                 isOperationClick = true;
                 break;
             case "-":
-                first = Integer.valueOf(textView.getText().toString());
+                first = Double.parseDouble(textView.getText().toString());
                 operation = "-";
                 isOperationClick = true;
                 break;
             case "X":
-                first = Integer.valueOf(textView.getText().toString());
+                first = Double.parseDouble(textView.getText().toString());
                 operation = "X";
                 isOperationClick = true;
                 break;
             case "/":
-                first = Integer.valueOf(textView.getText().toString());
+                first = Double.parseDouble(textView.getText().toString());
                 operation = "/";
                 isOperationClick = true;
                 break;
             case "%":
-                first = Integer.valueOf(textView.getText().toString());
+                first = Double.parseDouble(textView.getText().toString());
                 operation = "%";
                 isOperationClick = true;
                 break;
             case ".":
-                first = Integer.valueOf(textView.getText().toString());
+                first = Double.parseDouble(textView.getText().toString());
                 operation = ".";
                 isOperationClick = true;
                 break;
+            case "+/-":
+              Double  curentValue = Double.parseDouble(textView.getText().toString());
+              Double newValue=-curentValue;
+              Double value=Double.parseDouble(String.valueOf(newValue));
+                operation = "+/-";
+                if (operation.equals("+/-")) {
+                    sum=newValue;
+                    textView.setText(cancelDouble(value));
+                    isOperationClick = true;
+                }
+                break;
             case "=":
-                second = Integer.valueOf(textView.getText().toString());
+                second = Double.parseDouble(textView.getText().toString());
                 if (operation.equals("+")) {
                     sum = first + second;
-                    textView.setText(sum.toString());
+                    textView.setText(cancelDouble(sum));
                 } else if (operation.equals("-")) {
                     sum = first - second;
-                    textView.setText(sum.toString());
+                    textView.setText(cancelDouble(sum));
                 } else if (operation.equals("X")) {
                     sum = first * second;
-                    textView.setText(sum.toString());
+                    textView.setText(cancelDouble(sum));
                 } else if (operation.equals("/")) {
                     sum = first / second;
-                    textView.setText(sum.toString());
+                    textView.setText(cancelDouble(sum));
                 } else if (operation.equals("%")) {
-                    sum = (first/100) * second;
-                    textView.setText(sum.toString());
-                }else if (operation.equals(".")) {
-                    textView.setText(textButton.toString());
+                    sum = (first/100)*second;
+                    textView.setText(cancelDouble(sum));
                 }
+
+
                 isOperationClick = true;
         }
     }
-
-//    public void clickPercent(View view) {
-//        if(operation == ""){
-//            String textButton = ((Button) view).getText().toString();
-//            double temp = Double.parseDouble(textButton) / 100;
-//            textButton = temp + "";
-//            textView.setText(textButton);
-//        }else{
-//
-//        }
-//    }
-}
-
-
-/*
-switch (textButton){
-            case "+":
-                first = Integer.valueOf(textView.getText().toString());
-                break;
-            case "-":
-                first = Integer.valueOf(textView.getText().toString());
-                break;
-            case "X":
-                first = Integer.valueOf(textView.getText().toString());
-                break;
-            case "/":
-                first = Integer.valueOf(textView.getText().toString());
-                break;
-            case"=":
-                second = Integer.valueOf(textView.getText().toString());
-                sum = first + second;
-                textView.setText(sum.toString());
-                break;
+    public String cancelDouble(Double number){
+        String text = number.toString();
+        if(text.substring(text.length()-2).equals(".0")){
+            return text.substring(0, text.length()-2);
+        }else{
+            return number.toString();
         }
-        isOperationClick = true;
     }
- */
+}
